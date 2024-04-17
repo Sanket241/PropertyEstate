70e6fda7f394d88b1b79276a83209ba43f5526e4
Sanket <sanketsharma241@gmail.com>
2024-04-17 14:14:54 +0530

    adding private route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,25 +5,30 @@ import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
-// import PrivateRoute from './components/PrivateRoute'
+import Profile from './pages/Profile'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
     <>
-    <BrowserRouter>
-    <Header />
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/sign-in" element={<SignIn />} />
-    <Route path="/sign-up" element={<SignUp />} />
-    
-    
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
 
 
-    </Routes>
-    
-    </BrowserRouter>
+
+
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
